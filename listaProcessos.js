ad13ff1e2d4276307c2396bad6802f687b17ca40
renatoraj <rayresjr@gmail.com>
2019-08-20 09:13:52 -0300

    Melhoria

--- listaProcessos.js
+++ listaProcessos.js
@@ -1,5 +1,20 @@
 function loadProcessos(){
-		$.ajax({url: "https://portal.conam.com.br/itapecerica/index.php",data:{"frm_documento_requerente":"872.001.128-87","frm_numero_processo":"21653","frm_ano_processo":"2019"}, success: function(result){
-		    console.log(resu3lt);
+	var listaJson = JSON.parse($("#jvalue").val());
+	$.each(listaJson,function(index){
+		var item = listaJson[index];
+		console.log(item.frm_documento_requerente);
+		console.log(item.frm_numero_processo);
+		console.log(item.frm_ano_processo);
+		$.ajax({url: "https://portal.conam.com.br/itapecerica/index.php",data:{
+			"frm_documento_requerente": item.frm_documento_requerente + "",
+			"frm_numero_processo": item.frm_numero_processo + "",
+			"frm_ano_processo":"20" + item.frm_ano_processo},
+			success: function(result){
+				console.log(result);
+			$("#htmlNovo").append(result);
 		  }});
-	}
+	})
+}
+$(document).ready(function() {
+	$("body div:first").append("<legend>Lista de itens</legend><textarea id='jvalue'></textarea></br><button id='sendLista' onclick='loadProcessos()'>Carregar lista</button></br><section id='htmlNovo'></section");
+});
